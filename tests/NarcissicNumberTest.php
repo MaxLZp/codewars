2941cc0b25175a99c513775ac4c910e9deeffe8d
MaxLZp <linenko.maxim@gmail.com>
2022-04-02 16:55:18 +0300

    6 kyu Does my number look big in this?

---
+++ tests/NarcissicNumberTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 6 kyu Does my number look big in this?
+ *
+ * A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+ *
+ * @link https://en.wikipedia.org/wiki/Narcissistic_number
+ *
+ * For example, take 153 (3 digits), which is narcisstic:
+ *
+ * 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+ *
+ * and 1652 (4 digits), which isn't:
+ *
+ * 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+ *
+ * The Challenge:
+ *
+ * Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+ *
+ * Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+ *
+ */
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+function narcissistic(int $value): bool {
+    $stringLength = strlen($value);
+    $sum = array_reduce(str_split($value), function($carry, $el) use ($stringLength) {
+        return $carry + pow((int)$el, $stringLength);
+    }, 0);
+    return $sum === $value;
+}
+
+class NarcissicNumberTest extends TestCase {
+    public function testExamples() {
+        $this->assertTrue(narcissistic(7), '7 is narcissistic');
+        $this->assertTrue(narcissistic(371), '371 is narcissistic');
+    }
+}
