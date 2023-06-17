7622c8a5c29c476ae3caae8bc6790ccdd234cd92
Maxim Linenko <linenko.maxim@gmail.com>
2023-06-17 15:27:00 +0300

    kata: 6 Consonant value

---
+++ tests/K7_ConsonantValue/DataProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tests\K7_ConsonantValue;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            ["zodiac", 26],
+            ["chruschtschov", 80],
+            ["khrushchev", 38],
+            ["strength", 57],
+            ["catchphrase", 73],
+            ["twelfthstreet", 103],
+            ["mischtschenkoana", 80],
+            ["zozz", 52],
+        ];
+    }
+}
