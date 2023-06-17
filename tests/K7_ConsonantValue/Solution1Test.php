cfb9335ba8559196b9dc78e9c14a68d4db27e5d2
Maxim Linenko <linenko.maxim@gmail.com>
2023-06-17 15:27:00 +0300

    kata: 6 Consonant value

---
+++ tests/K7_ConsonantValue/Solution1Test.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace tests\K7_ConsonantValue;
+
+use PHPUnit\Framework\TestCase;
+
+class Solution1Test extends TestCase
+{
+
+    /**
+     * @test
+     * @dataProvider \tests\K7_ConsonantValue\DataProvider::data
+     */
+    public function execute($input, $expected) {
+        $this->assertSame($expected, $this->solve($input));
+    }
+
+    public function solve($input): int
+    {
+        $vowels = ['a','e','i','o','u'];
+        $max = 0;
+        $current = 0;
+        foreach (str_split($input) as $char) {
+            if (!in_array($char, $vowels)) {
+                $current += ord($char) - ord('a') + 1;
+                if ($current > $max) {
+                    $max = $current;
+                }
+                continue;
+            }
+            $current = 0;
+        }
+        return $max;
+    }
+}
