83feee6c752c07b8ae036aa7fbb3726b0cd48dc5
Maxim Linenko <linenko.maxim@gmail.com>
2023-06-17 12:09:42 +0300

    kata: 7 Digit*Digit

---
+++ tests/K7_DigitDigit/DataProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\K7_DigitDigit;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [9119, 811181, ],
+            [24680, 41636640, ],
+            [13579, 19254981, ],
+            [0, 0, ],
+        ];
+    }
+}
