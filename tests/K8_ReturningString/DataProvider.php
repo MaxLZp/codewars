30cf1580ab4c7d7f37aba2d81695d09f440f18a9
Maxim Linenko <linenko.maxim@gmail.com>
2023-06-18 13:00:27 +0300

    kata: 8 Returning String

---
+++ tests/K8_ReturningString/DataProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\K8_ReturningString;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            ['Ryan', "Hello, Ryan how are you doing today?"],
+            ['sForuIhDxDT', "Hello, sForuIhDxDT how are you doing today?"],
+            ['iKeNFW uQ', "Hello, iKeNFW uQ how are you doing today?"],
+            ['SabDaefSMIJGO', "Hello, SabDaefSMIJGO how are you doing today?"],
+        ];
+    }
+}
