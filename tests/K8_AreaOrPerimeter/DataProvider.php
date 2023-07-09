33d8d890d4229355ad78b8c2461f80cbe3000e23
Maxim Linenko <linenko.maxim@gmail.com>
2023-07-09 12:04:45 +0300

    kata: php-8kyu AreaOrPerimeter

---
+++ tests/K8_AreaOrPerimeter/DataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\K8_AreaOrPerimeter;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[3,3], 9],
+            [[3,7], 20],
+        ];
+    }
+}
