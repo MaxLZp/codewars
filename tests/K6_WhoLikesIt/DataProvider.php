d7ac6054109ed0153ab4aeb2c61dfcad16a1cf96
Maxim Linenko <linenko.maxim@gmail.com>
2023-07-15 11:52:24 +0300

    kata: php-6kyu Who likes it

---
+++ tests/K6_WhoLikesIt/DataProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\K6_WhoLikesIt;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[], 'no one likes this'],
+            [[ 'Peter' ], 'Peter likes this'],
+            [[ 'Jacob', 'Alex' ], 'Jacob and Alex like this'],
+            [[ 'Max', 'John', 'Mark' ], 'Max, John and Mark like this'],
+            [[ 'Alex', 'Jacob', 'Mark', 'Max' ], 'Alex, Jacob and 2 others like this'],
+        ];
+    }
+}
