af62d791d417a5ca7e3a3c8aaf9658b711049a45
Maxim Linenko <linenko.maxim@gmail.com>
2023-07-22 09:57:05 +0300

    kata: php-8kyu Simple multiplication

---
+++ tests/K8_SimpleMultiplication/DataProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\K8_SimpleMultiplication;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [2, 16],
+            [1, 9],
+            [8, 64],
+            [4, 32],
+            [5, 45],
+        ];
+    }
+}
---
+++ tests/K8_SimpleMultiplication/description.md
@@ -0,0 +1,6 @@
+# Simple multiplication
+## 8 kyu
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
