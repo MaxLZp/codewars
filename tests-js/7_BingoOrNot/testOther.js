650ec8ac538971d63cd9cc017ee597a8ef8af512
Maxim Linenko <linenko.maxim@gmail.com>
2023-08-02 08:41:24 +0300

    kata: js-7kyu Bingo (or not)

---
+++ tests-js/7_BingoOrNot/testOther.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+describe('Kyu 7: Count consonants', function () {
+
+  function bingo(a) {
+      return 'bingo'
+        .split('')
+        .map((c) => {return c.charCodeAt(0) - 'a'.charCodeAt(0) + 1})
+        .every(bn => a.includes(bn)) ? "WIN" : "LOSE";
+  }
+
+  it("Empty string", function() {
+    assert.equal(bingo([1,2,3,4,5,6,7,8,9,10]), "LOSE");
+    assert.equal(bingo([21,13,2,7,5,14,7,15,9,10]), "WIN");
+  });
+
+});
