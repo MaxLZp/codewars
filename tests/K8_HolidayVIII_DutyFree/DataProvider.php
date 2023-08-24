6b40e1e049b0b578152b82eb66fae674def08285
Maxim Linenko <linenko.maxim@gmail.com>
2023-08-24 08:16:04 +0300

    kata: 8kyu Holiday VIII - Duty Free

---
+++ tests/K8_HolidayVIII_DutyFree/DataProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\K8_HolidayVIII_DutyFree;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[12, 50, 1000], 166],
+            [[17, 10, 500], 294],
+            [[24, 35, 3000], 357],
+        ];
+    }
+}
