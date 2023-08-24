2a624b22a55da6d892a8a2fe9d96e311d21eb437
Maxim Linenko <linenko.maxim@gmail.com>
2023-08-24 08:16:04 +0300

    kata: 8kyu Holiday VIII - Duty Free

---
+++ tests/K8_HolidayVIII_DutyFree/Solution1Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tests\K8_HolidayVIII_DutyFree;
+
+use PHPUnit\Framework\TestCase;
+
+class Solution1Test extends TestCase
+{
+
+    /**
+     * @test
+     * @dataProvider \tests\K8_HolidayVIII_DutyFree\DataProvider::data
+     */
+    public function execute($input, $expected) {
+        $this->assertEquals($expected, $this->solve(...$input));
+    }
+
+    public function solve(int $n, int $d, int $h): int
+    {
+        return $h / ( $n * ($d/100));
+    }
+}
