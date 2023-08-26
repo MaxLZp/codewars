204a8fb058a7bd7d2ab1f74a069bcae1d5fd8380
Maxim Linenko <linenko.maxim@gmail.com>
2023-08-26 19:18:52 +0300

    kata: 7 kyu Jaden Casing Strings

---
+++ tests/K7_JadenCasingStrings/DataProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\K7_JadenCasingStrings;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [
+                "How can mirrors be real if our eyes aren't real",
+                "How Can Mirrors Be Real If Our Eyes Aren't Real"
+            ],
+        ];
+    }
+}
