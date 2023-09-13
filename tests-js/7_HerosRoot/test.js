a2a27e5ec793306b2784ee8af46ea0aa05e09e84
Maxim Linenko <linenko.maxim@gmail.com>
2023-09-13 09:44:48 +0300

    kata: js-7kyu Hero's root    Some tests fail

---
+++ tests-js/7_HerosRoot/test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+describe('Kyu 7: Hero\'s root', function () {
+
+  it('solution1', function () {
+    function intRac(n, guess) {
+      let step = 0;
+      let pguess = guess;
+      do {
+        step++;
+        pguess = guess;
+        guess = Math.floor(( guess + n/guess ) / 2);
+      } while ( Math.abs(pguess - guess) >= 1 )
+      return step;
+    };
+
+    assert.strictEqual(intRac(1, 1), 1)
+    assert.strictEqual(intRac(25, 1), 4)
+    assert.strictEqual(intRac(125348, 300), 3)
+
+    assert.strictEqual(intRac(26, 1), 5)
+  });
+
+});
