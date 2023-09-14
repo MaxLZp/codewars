a39677a5a8a87fd4323ae15f92e660e58bcd544a
Maxim Linenko <linenko.maxim@gmail.com>
2023-09-14 09:07:57 +0300

    kata: php-7kyu Hero's root

---
+++ tests/K7_HerosRoot/DataProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\K7_HerosRoot;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[25, 1],4],
+            [[125348, 300],3],
+            [[236, 12],2],
+            [[48981764, 8000],3],
+        ];
+    }
+}
