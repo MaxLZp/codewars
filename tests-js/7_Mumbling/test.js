1684f510f47fdce05b8c98cb285224c3be0db05a
Maxim Linenko <linenko.maxim@gmail.com>
2023-09-27 08:23:33 +0300

    kata: js-7kyu Mumbling

---
+++ tests-js/7_Mumbling/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe('Kyu 7: Mumbling', function () {
+
+  function accum(s) {
+    // your code
+    return [...s].map((value, index) => {
+      return value.toUpperCase() + value.toLowerCase().repeat(index);
+    }).join('-');
+  }
+
+  assert.equal(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+	assert.equal(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+	assert.equal(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+	assert.equal(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+	assert.equal(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+
+});
