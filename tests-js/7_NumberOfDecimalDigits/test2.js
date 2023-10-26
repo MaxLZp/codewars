ecce1caf9d3714eeb91cbcbebac8ec6ab00a371a
Maxim Linenko <linenko.maxim@gmail.com>
2023-10-26 08:33:48 +0300

    kata: ks-7kyu Number of Decimal Digits

---
+++ tests-js/7_NumberOfDecimalDigits/test2.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+
+describe('Kyu 7: Number of Decimal Digits 2', function () {
+
+    function digits(n) {
+      return `${n}`.length;
+    }
+
+    const tests = [
+      [0, 1],
+      [9, 1],
+      [66, 2],
+      [12345, 5],
+      [128685, 6],
+      [9876543210, 10],
+      [9007199254740991, 16],
+    ];
+
+    for (let [n, expected] of tests) {
+      it(`${n}`, function() {
+        assert.strictEqual(digits(n), expected);
+      });
+    }
+
+});
