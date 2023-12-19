f97ff92dbc7817750a5990748f2dfcafa3ed5189
Maxim Linenko <linenko.maxim@gmail.com>
2023-12-19 08:21:48 +0200

    kata: php-7kyu Calculate Meal Total

---
+++ tests/K7_CalculateMealTotal/DataProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\K7_CalculateMealTotal;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [ [5, 5, 10], 5.75, ]
+        ];
+    }
+}
