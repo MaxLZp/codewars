034c3c0cd04d35afb55c6c8c95b93b4dc9391416
Maxim Linenko <linenko.maxim@gmail.com>
2024-01-06 11:23:54 +0200

    kata: php-6kyu Meeting

---
+++ tests/K6_Meeting/DataProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tests\K6_Meeting;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [
+                "Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:KERN;Megan:Stan;Alex:Korn",
+                "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)"
+            ],
+            [
+                "John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell",
+                "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)"
+            ],
+        ];
+    }
+}
