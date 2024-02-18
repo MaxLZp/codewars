d00f1dee48834ae29732bdbd16725a89c8e14072
Maxim Linenko <linenko.maxim@gmail.com>
2024-02-18 11:42:16 +0200

    kata: php-6kyu Maze Runner

---
+++ tests/K6_MazeRunner/DataProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace tests\K6_MazeRunner;
+
+class DataProvider
+{
+    private $maze =
+    [[1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,3],
+    [1,0,1,0,1,0,1],
+    [0,0,1,0,0,0,1],
+    [1,0,1,0,1,0,1],
+    [1,0,0,0,0,0,1],
+    [1,2,1,0,1,0,1]];
+
+    public function data()
+    {
+        return [
+            [[$this->maze,["N","N","N","N","N","E","E","E","E","E"]], "Finish",],
+            [[$this->maze,["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]], "Finish",],
+            [[$this->maze,["N","N","N","N","N","E","E","E","E","E","W","W"]], "Finish",],
+            [[$this->maze,["N","N","N","W","W"]], "Dead",],
+            [[$this->maze,["N","N","N","N","N","E","E","S","S","S","S","S","S"]], "Dead",],
+            [[$this->maze,["N","E","E","E","E"]], "Lost",],
+        ];
+    }
+}
