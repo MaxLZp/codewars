831946089221f7ae23b9bfb1d5d90f14c439eb34
Maxim Linenko <linenko.maxim@gmail.com>
2024-04-21 16:53:05 +0300

    kata: php-6kyu Piano Kata, Part 2

---
+++ tests/K6_PianoKata_Part2/DataProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tests\K6_PianoKata_Part2;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [1, "A"],
+            [5, "C#"],
+            [12, "G#"],
+            [42, "D"],
+            [88, "C"],
+            [89, "A"],
+            [92, "C"],
+            [100, "G#"],
+            [111, "G"],
+            [200, "G#"],
+            [2017, "F"],
+        ];
+    }
+}
