37be287e32a2439ebee63cf90984e4b333e7d963
Maxim Linenko <linenko.maxim@gmail.com>
2024-08-11 07:20:44 +0300

    kata: php-7kyu Hungarian Vowel Harmony (easy)

---
+++ tests/K7_HungraianVowelHarmony_easy/DataProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\K7_HungraianVowelHarmony_easy;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            ["ablak", "ablaknak"],
+            ["szék", "széknek"],
+            ["otthon", "otthonnak"],
+
+            ["tett", "tettnek"],
+        ];
+    }
+}
