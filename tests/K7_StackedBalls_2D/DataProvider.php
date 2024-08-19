53d3d3d2a90bc69a245952492e9195b0d83588fa
Maxim Linenko <linenko.maxim@gmail.com>
2024-08-19 07:42:40 +0300

    kata: php-7kyu Stacked Balls - 2D

---
+++ tests/K7_StackedBalls_2D/DataProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\K7_StackedBalls_2D;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [0, 0.0],
+            [1, 1.0],
+            [2, 1.866],
+        ];
+    }
+}
