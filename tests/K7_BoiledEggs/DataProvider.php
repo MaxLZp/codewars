a9b520a8d52b3468c5d38241d3b2f5ec77987982
Maxim Linenko <linenko.maxim@gmail.com>
2024-09-24 07:47:29 +0300

    kata: php-7kyu Boiled Eggs

---
+++ tests/K7_BoiledEggs/DataProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests\K7_BoiledEggs;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [0, 0],
+            [5, 5],
+            [10, 10],
+            [0, 0],
+            [1, 5],
+            [5, 5],
+            [8, 5],
+            [9, 10],
+            [10, 10],
+            [16, 10],
+            [20, 15],
+            [100, 65],
+        ];
+    }
+}
