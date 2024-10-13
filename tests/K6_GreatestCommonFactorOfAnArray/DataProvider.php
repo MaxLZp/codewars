89677c2d132c883974c17985daabbf87d78c5c44
Maxim Linenko <linenko.maxim@gmail.com>
2024-10-13 10:34:26 +0300

    kata: php-6kyu Greatest Common Factor of an Array

---
+++ tests/K6_GreatestCommonFactorOfAnArray/DataProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\K6_GreatestCommonFactorOfAnArray;
+
+class DataProvider
+{
+
+    public function data()
+    {
+        return [
+            [[46, 14, 20, 88], 2],
+
+            [[171, 45, 297, 342], 9],
+            [[40, 60, 100], 20],
+        ];
+    }
+}
