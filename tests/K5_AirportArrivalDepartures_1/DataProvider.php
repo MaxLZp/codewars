231c741f26dc5fc18db56a740fb1c8c6593475eb
Maxim Linenko <linenko.maxim@gmail.com>
2024-10-26 10:14:15 +0300

    kata: php-5kyu Airport Arrivals/Departures - 1

---
+++ tests/K5_AirportArrivalDepartures_1/DataProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\K5_AirportArrivalDepartures_1;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[["CAT"], [[1, 13, 27]]], ["DOG"]],
+        ];
+    }
+}
