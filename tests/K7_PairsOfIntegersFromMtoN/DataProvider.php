802578c63fa86442327a4441c1573bdcef69fde3
Maxim Linenko <linenko.maxim@gmail.com>
2024-10-28 07:25:10 +0200

    kata: php-7kyu Pairs of integers from m to n

---
+++ tests/K7_PairsOfIntegersFromMtoN/DataProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\K7_PairsOfIntegersFromMtoN;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [
+                [2,4],
+                [ [2, 2], [2, 3], [2, 4], [3, 3], [3, 4], [4, 4] ]
+            ],
+        ];
+    }
+}
