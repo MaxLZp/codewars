dc7da758e72e9e1da973c9cbee47841b5b8ae39b
Maxim Linenko <linenko.maxim@gmail.com>
2024-11-11 07:43:53 +0200

    kata: php-7kyu Perimeter sequence

---
+++ tests/K7_PerimeterSequence/Solution1Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tests\K7_PerimeterSequence;
+
+use PHPUnit\Framework\TestCase;
+use tests\K7_OOP4_PeoplePeoplePeople_Practice\Person;
+
+class Solution1Test extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function execute($input, $expected) {
+        $this->assertSame(12, $this->solve(1, 3));
+    }
+
+    public function solve(int $a, int $n): int
+    {
+        return 4 * $a * $n;
+    }
+}
