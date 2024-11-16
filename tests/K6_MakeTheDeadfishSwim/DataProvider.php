183872966f66a7864af4c84fabddf92f50815c96
Maxim Linenko <linenko.maxim@gmail.com>
2024-11-16 10:37:11 +0200

    kata: 6kyu Make the Deadfish Swim

---
+++ tests/K6_MakeTheDeadfishSwim/DataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\K6_MakeTheDeadfishSwim;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+             ["iiisdoso", [ 8, 64 ]],
+             ["iiisxxxdoso", [ 8, 64 ]],
+        ];
+    }
+}
