f9e507794fb77539880bc63f95c1a6fc87f41632
Maxim Linenko <linenko.maxim@gmail.com>
2024-11-18 07:43:59 +0200

    kata: php-7kyu Two Oldest Ages

---
+++ tests/K7_TwoOldestAges/DataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\K7_TwoOldestAges;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[1,5,87,45,8,8], [45, 87]],
+            [[6,5,83,5,3,18], [18, 83]],
+        ];
+    }
+}
