7b8e78306656422d1683ca147d75bc115fd58234
Maxim Linenko <linenko.maxim@gmail.com>
2024-11-24 10:54:05 +0200

    kata: php-6kyu Almost Even

---
+++ tests/K6_AlmostEven/DataProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\K6_AlmostEven;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[20, 6], [ 3, 3, 3, 3, 4, 4 ]],
+        ];
+    }
+}
---
+++ tests/K6_AlmostEven/Solution1Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tests\K6_AlmostEven;
+
+use PHPUnit\Framework\TestCase;
+
+class Solution1Test extends TestCase
+{
+
+    /**
+     * @test
+     * @dataProvider \tests\K6_AlmostEven\DataProvider::data
+     */
+    public function execute($input, $expected) {
+        $this->assertSame($expected, $this->solve($input[0], $input[1]));
+    }
+
+
+    public function solve(int $num, int $parts)
+    {
+        $val = intval($num / $parts);
+        $rest = $num % $parts;
+
+        return array_merge(
+            array_fill(0, $parts - $rest, $val),
+            array_fill(0, $rest, $val + 1)
+        );
+    }
+
+}
