ee8da0e9e94c2f76f5eccbeca12bf6034b4a0e3f
Maxim Linenko <linenko.maxim@gmail.com>
2024-12-14 07:26:14 +0200

    kata: js-7kyu The Four Seasons

---
+++ tests-js/7_TheFourSeasons/test.js
@@ -0,0 +1,27 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe('Kyu 7: The Four Seasons', function () {
+
+  function fourSeasons(d) {
+    if (d > 365) { return 'The year flew by!'; }
+    if (d < 80) { return 'Winter Season';}
+    if (d < 172) { return 'Spring Season';}
+    if (d < 264) { return 'Summer Season';}
+    if (d < 355) { return 'Autumn Season';}
+    if (d <= 365) { return 'Winter Season';}
+  }
+
+  it("tests",function() {
+    assert.strictEqual(fourSeasons(236),'Summer Season');
+    assert.strictEqual(fourSeasons(109),'Spring Season');
+    assert.strictEqual(fourSeasons(312),'Autumn Season');
+    assert.strictEqual(fourSeasons(1),'Winter Season');
+    assert.strictEqual(fourSeasons(356),'Winter Season');
+    assert.strictEqual(fourSeasons(400),'The year flew by!');
+
+    assert.strictEqual(fourSeasons(80),'Autumn Season');
+  })
+
+});
