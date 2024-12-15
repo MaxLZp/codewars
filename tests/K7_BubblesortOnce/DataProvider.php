c942b0f67e90be1c79ac5718e389501f047ed2f7
Maxim Linenko <linenko.maxim@gmail.com>
2024-12-15 07:57:08 +0200

    kata: php-7kyu Bubblesort Once

---
+++ tests/K7_BubblesortOnce/DataProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\K7_BubblesortOnce;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [array(9, 7, 5, 3, 1, 2, 4, 6, 8), array(7, 5, 3, 1, 2, 4, 6, 8, 9)],
+        ];
+    }
+}
