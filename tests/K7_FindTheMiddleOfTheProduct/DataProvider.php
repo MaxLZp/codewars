b4f7e03e73403566495e61a8c01a243d020c4ac9
Maxim Linenko <linenko.maxim@gmail.com>
2025-01-10 07:21:39 +0200

    kata: php-7kyu Find the Middle of the Product

---
+++ tests/K7_FindTheMiddleOfTheProduct/DataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\K7_FindTheMiddleOfTheProduct;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            ['s7d8jd9', 0],
+            ['58jd9fgh/fgh6s.,sdf', 16],
+        ];
+    }
+}
