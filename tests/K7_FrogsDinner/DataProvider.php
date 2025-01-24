9a82ee3d7dee1ab4542c278be59e6ece95ecbe97
Maxim Linenko <linenko.maxim@gmail.com>
2025-01-24 08:04:49 +0200

    kata: php-7kyu Frog's Dinner

---
+++ tests/K7_FrogsDinner/DataProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\K7_FrogsDinner;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [5, "Chris ate 15 flies, Tom ate 28 flies and Cat ate 946 flies"],
+            [8, "Chris ate 36 flies, Tom ate 171 flies and Cat ate 21528 flies"],
+            [0, "Chris ate 0 flies, Tom ate 0 flies and Cat ate 0 flies"],
+        ];
+    }
+}
