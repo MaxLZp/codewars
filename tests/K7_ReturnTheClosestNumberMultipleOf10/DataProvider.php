734845f14dd6bbe8d254b1eae8027743ae73f73e
Maxim Linenko <linenko.maxim@gmail.com>
2025-02-12 07:14:48 +0200

    kata: php-7kyu Return the closest number multiple of 10

---
+++ tests/K7_ReturnTheClosestNumberMultipleOf10/DataProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\K7_ReturnTheClosestNumberMultipleOf10;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [22, 20],
+            [25, 30],
+            [37, 40],
+            [54, 50],
+            [55, 60],
+        ];
+    }
+}
---
+++ tests/K7_ReturnTheClosestNumberMultipleOf10/description.md
@@ -0,0 +1,18 @@
+# Return the closest number multiple of 10
+## 7 kyu
+
+Given a number return the closest number to it that is divisible by 10.
+
+Example input:
+```
+22
+25
+37
+```
+
+Expected output:
+```
+20
+30
+40
+```
