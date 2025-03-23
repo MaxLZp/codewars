474d2361abf498fdf311cb4a5821285811b20ad6
Maxim Linenko <linenko.maxim@gmail.com>
2025-03-23 08:43:06 +0200

    kata: php-7kyu Cats and shelves

---
+++ tests/K7_CatsAndShelves/DataProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\K7_CatsAndShelves;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [1, 5, 2],
+            [1, 1, 0],
+            [2, 5, 1],
+            [2, 4, 2],
+
+            [1, 7, 2],
+            [89, 95, 2],
+        ];
+    }
+}
