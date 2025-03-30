518154df8faffcb7a6600fd90ba93c4c849a0695
Maxim Linenko <linenko.maxim@gmail.com>
2025-03-30 10:37:55 +0300

    kata: php-7kyu Char Code Calculation

---
+++ tests/K7_CharCodeCalculation/DataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\K7_CharCodeCalculation;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            ['ABC', 6],
+            ['abcdef', 6],
+        ];
+    }
+}
