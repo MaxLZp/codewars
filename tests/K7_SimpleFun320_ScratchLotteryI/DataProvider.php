12bae90f4809bfcbf66ac1e0800827fa8ef487f6
Maxim Linenko <linenko.maxim@gmail.com>
2025-05-04 08:34:03 +0300

    kata: php-7kyu Simple Fun 320: Scratch lottery I

---
+++ tests/K7_SimpleFun320_ScratchLotteryI/DataProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tests\K7_SimpleFun320_ScratchLotteryI;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [[
+                "tiger tiger tiger 100",
+                "rabbit dragon snake 100",
+                "rat ox pig 1000",
+                "dog cock sheep 10",
+                "horse monkey rat 5",
+                "dog dog dog 1000"
+            ], 1100],
+        ];
+    }
+}
