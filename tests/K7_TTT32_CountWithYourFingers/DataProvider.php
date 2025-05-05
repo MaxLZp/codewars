f8da29b2617e6104ef204e2dc76f3b8b902dc26e
Maxim Linenko <linenko.maxim@gmail.com>
2025-05-05 07:47:45 +0300

    kata: php-7kyu T.T.T.32: Count with your fingers

---
+++ tests/K7_TTT32_CountWithYourFingers/DataProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\K7_TTT32_CountWithYourFingers;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [10, "Index finger"],
+            [20, "Ring finger"],
+            [30, "Ring finger"],
+            [50, "Index finger"],
+            [100, "Ring finger"],
+            [1000, "Index finger"],
+            [10000, "Index finger"],
+        ];
+    }
+}
