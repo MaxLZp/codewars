f21cbc0f77856ca800c4a18a2bb2efda6d3e917b
Maxym Linenko <linenko.maxim@gmail.com>
2025-06-21 09:12:09 +0300

    kata:js-7kyu Binary Representation of an Integer

---
+++ tests-js/7_BinaryRepresentationOfABinary/test.js
@@ -0,0 +1,35 @@
+var { assert } = require('chai');
+
+describe('Kyu 7: Binary Representation of an Integer', function () {
+
+  function showBits (n) {
+    return (n >>> 0).toString(2).padStart(32, '0').split('').map(Number);
+  }
+
+  function doTest (input, expected) {
+    const actual = showBits(input);
+    assert.deepEqual(actual, expected, `for n = ${input}:\n`);
+  }
+
+	it('sample tests', function () {
+		doTest(211,
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1]
+		);
+		doTest(-1,
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		);
+		doTest(701,
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1]
+		);
+		doTest(0,
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		);
+		doTest(2 ** 31 - 1,
+			[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		);
+		doTest(-(2 ** 31),
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		);
+	});
+
+});
