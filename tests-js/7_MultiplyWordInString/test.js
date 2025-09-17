e198622a840824daf7098f292ef6abc12a9ce09c
Maxym Linenko <linenko.maxim@gmail.com>
2025-09-17 07:06:25 +0300

    kata: js-7kyu Multiply Word in String

---
+++ tests-js/7_MultiplyWordInString/test.js
@@ -0,0 +1,25 @@
+const { assert } = require('chai');
+
+describe('Kyu 7: Multiply Word in String', function () {
+
+  function modifyMultiply (str,loc,num) {
+    var words = str.match(/\b[a-z\-]+\b/ig);
+    if(loc >= words.length) {return '';}
+    var result = '';
+    for(var i = 0; i < num-1; i++) {
+      result += `${words[loc]}-`;
+    }
+    result += words[loc];
+
+    return result;
+  }
+
+  it("Basic Test Cases", function() {
+    assert.strictEqual(modifyMultiply("This is a string", 3, 5), "string-string-string-string-string", "The string is incorrect");
+    assert.strictEqual(modifyMultiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.", 8, 10), "that-that-that-that-that-that-that-that-that-that");
+    assert.strictEqual(modifyMultiply("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence", 1, 1), "means");
+    assert.strictEqual(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.", 6, 8), "ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance");
+    assert.strictEqual(modifyMultiply("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.", 2, 5), "around-around-around-around-around");
+  });
+
+});
