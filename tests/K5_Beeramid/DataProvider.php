02410d5e3dc846f91fa285ec827c8338889aebed
Maxym Linenko <linenko.maxim@gmail.com>
2025-11-18 07:09:27 +0200

    kata: php_python-5kyu Beeramid

---
+++ tests/K5_Beeramid/DataProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\K5_Beeramid;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+             [[9, 2], 1,],
+             [[21, 1.5], 3,],
+             [[-1, 4], 0,],
+        ];
+    }
+}
