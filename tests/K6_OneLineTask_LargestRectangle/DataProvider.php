6ac17cc72470854302a314ee5d87eaf67c544df4
Maxym Linenko <linenko.maxim@gmail.com>
2026-01-11 08:37:40 +0200

    kata: php-6kyu One Line Task: Largest Rectangle

---
+++ tests/K6_OneLineTask_LargestRectangle/DataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\K6_OneLineTask_LargestRectangle;
+
+class DataProvider
+{
+    public function data()
+    {
+        return [
+            [100, [25, 25]],
+            [10, [2.5, 2.5]],
+        ];
+    }
+}
