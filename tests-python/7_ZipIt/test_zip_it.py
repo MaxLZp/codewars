d5908f96a5647c6d64d55df4ec79a3f6a3af6064
Maxym Linenko <linenko.maxim@gmail.com>
2026-07-03 07:58:03 +0300

    kata: python-7kyu Zip it

---
+++ tests-python/7_ZipIt/test_zip_it.py
@@ -0,0 +1,12 @@
+def lstzip(a,b,fn):
+    return [fn(x,y) for x,y in zip(a, b)]
+    
+
+def test_zip_it():
+    a = [1, 2, 3, 4, 5]
+    b = ['a','b','c','d','e']
+    assert lstzip(a, b, lambda a, b: str(a) + str(b)), ["1a", "2b", "3c", "4d", "5e"]
+    assert lstzip(b, a, lambda a, b: str(a) + str(b)), ["a1", "b2", "c3", "d4", "e5"]
+    assert lstzip(b, lstzip(a, b, lambda a, b: a * ord(b[0])), lambda a, b: str(a) + str(b)), ["a97", "b196", "c297", "d400", "e505"]
+
+    
